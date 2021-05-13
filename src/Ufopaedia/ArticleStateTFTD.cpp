d56685b49633989f6d5b9fde3ce509c2a3c50a23
Ng Kock Leong <drago888@gmail.com>
2021-05-13 15:20:01 +0800

    Fix the TFTD Article State for "On Building Hideouts" Lore

--- src/Ufopaedia/ArticleStateTFTD.cpp
+++ src/Ufopaedia/ArticleStateTFTD.cpp
@@ -33,6 +33,11 @@ namespace OpenXcom
 
 	ArticleStateTFTD::ArticleStateTFTD(ArticleDefinitionTFTD *defs, std::shared_ptr<ArticleCommonState> state) : ArticleState(defs->id, std::move(state))
 	{
+		int bpp = Options::pediaBgResolutionX == Screen::ORIGINAL_WIDTH ? 8 : 32;
+		int scaleX = Options::pediaBgResolutionX / Screen::ORIGINAL_WIDTH;
+		int scaleY = Options::pediaBgResolutionY / Screen::ORIGINAL_HEIGHT;
+		SDL_Color* buttonTextPalette = _game->getMod()->getPalettes().find("PAL_BATTLEPEDIA")->second->getColors();
+
 		RuleInterface *ruleInterface;
 		switch (defs->getType())
 		{
@@ -65,25 +70,35 @@ namespace OpenXcom
 				break;
 		}
 
-		// Set palette
-		if (defs->customPalette)
-		{
-			if (ruleInterface->getPalette() == "PAL_GEOSCAPE")
-				_cursorColor = Mod::GEOSCAPE_CURSOR;
-			else if (ruleInterface->getPalette() == "PAL_BASESCAPE")
-				_cursorColor = Mod::BASESCAPE_CURSOR;
-			else if (ruleInterface->getPalette() == "PAL_UFOPAEDIA")
-				_cursorColor = Mod::UFOPAEDIA_CURSOR;
-			else if (ruleInterface->getPalette() == "PAL_GRAPHS")
-				_cursorColor = Mod::GRAPHS_CURSOR;
-			else
-				_cursorColor = Mod::BATTLESCAPE_CURSOR;
 
-			setCustomPalette(_game->getMod()->getSurface(defs->image_id)->getPalette(), _cursorColor);
+
+		if (bpp == 8)
+		{
+			// Set palette
+			if (defs->customPalette)
+			{
+				if (ruleInterface->getPalette() == "PAL_GEOSCAPE")
+					_cursorColor = Mod::GEOSCAPE_CURSOR;
+				else if (ruleInterface->getPalette() == "PAL_BASESCAPE")
+					_cursorColor = Mod::BASESCAPE_CURSOR;
+				else if (ruleInterface->getPalette() == "PAL_UFOPAEDIA")
+					_cursorColor = Mod::UFOPAEDIA_CURSOR;
+				else if (ruleInterface->getPalette() == "PAL_GRAPHS")
+					_cursorColor = Mod::GRAPHS_CURSOR;
+				else
+					_cursorColor = Mod::BATTLESCAPE_CURSOR;
+
+				setCustomPalette(_game->getMod()->getSurface(defs->image_id)->getPalette(), _cursorColor);
+			}
+			else
+			{
+				setStandardPalette(ruleInterface->getPalette());
+			}
 		}
 		else
 		{
-			setStandardPalette(ruleInterface->getPalette());
+			genPediaPal();
+			_cursorColor = Mod::UFOPAEDIA_CURSOR;
 		}
 
 		_buttonColor = ruleInterface->getElement("button")->color;
@@ -101,40 +116,63 @@ namespace OpenXcom
 			_ammoColor = ruleInterface->getElement("ammoColor")->color;
 		}
 
-		_btnInfo->setX(183);
-		_btnInfo->setY(179);
-		_btnInfo->setHeight(10);
-		_btnInfo->setWidth(40);
+		_btnInfo->setX(183 * scaleX);
+		_btnInfo->setY(179 * scaleY);
+		_btnInfo->setHeight(10 * scaleY);
+		_btnInfo->setWidth(40 * scaleX);
 		_btnInfo->setColor(_buttonColor);
-		_btnOk->setX(227);
-		_btnOk->setY(179);
-		_btnOk->setHeight(10);
-		_btnOk->setWidth(23);
+		_btnOk->setX(227 * scaleX);
+		_btnOk->setY(179 * scaleY);
+		_btnOk->setHeight(10 * scaleY);
+		_btnOk->setWidth(23 * scaleX);
 		_btnOk->setColor(_buttonColor);
-		_btnPrev->setX(254);
-		_btnPrev->setY(179);
-		_btnPrev->setHeight(10);
-		_btnPrev->setWidth(23);
+		_btnPrev->setX(254 * scaleX);
+		_btnPrev->setY(179 * scaleY);
+		_btnPrev->setHeight(10 * scaleY);
+		_btnPrev->setWidth(23 * scaleX);
 		_btnPrev->setColor(_buttonColor);
-		_btnNext->setX(281);
-		_btnNext->setY(179);
-		_btnNext->setHeight(10);
-		_btnNext->setWidth(23);
+		_btnNext->setX(281 * scaleX);
+		_btnNext->setY(179 * scaleY);
+		_btnNext->setHeight(10 * scaleY);
+		_btnNext->setWidth(23 * scaleX);
 		_btnNext->setColor(_buttonColor);
 
+		if (bpp != 8) {
+			_btnOk->statePalette = _palette;
+			_btnOk->setTextPalette(buttonTextPalette);
+			_btnPrev->statePalette = _palette;
+			_btnPrev->setTextPalette(buttonTextPalette);
+			_btnNext->statePalette = _palette;
+			_btnNext->setTextPalette(buttonTextPalette);
+			_btnInfo->statePalette = _palette;
+			_btnInfo->setTextPalette(buttonTextPalette);
+		}
 		ArticleState::initLayout();
 
 		// Step 1: background image
 		if (!defs->customPalette)
 		{
-			_game->getMod()->getSurface(ruleInterface->getBackgroundImage())->blitNShade(_bg, 0, 0);
+			if (bpp == 8) {
+				_game->getMod()->getSurface(ruleInterface->getBackgroundImage())->blitNShade(_bg, 0, 0);
+			}
+			else {
+				Surface surf;
+				get32Surf("32_" + ruleInterface->getBackgroundImage(), ruleInterface->getBackgroundImage(), &surf, "PAL_BATTLEPEDIA")->blitNShade32(_bg, 0, 0, -30); //brighten the background
+			}
 		}
 
 		// Step 2: article image (optional)
 		Surface *image = _game->getMod()->getSurface(defs->image_id, false);
 		if (image)
 		{
-			image->blitNShade(_bg, 0, 0);
+			if (bpp == 8) {
+				image->blitNShade(_bg, 0, 0);
+			}
+			else {
+				Surface surf;
+				get32Surf("32_" + defs->image_id, defs->image_id, &surf, "PAL_BATTLEPEDIA")->blitNShade32(_bg, 0, 0, -30); //brighten the background
+			}
+
 		}
 
 		// Step 3: info button image
@@ -149,29 +187,37 @@ namespace OpenXcom
 				case UFOPAEDIA_TYPE_TFTD_CRAFT:
 				case UFOPAEDIA_TYPE_TFTD_CRAFT_WEAPON:
 				case UFOPAEDIA_TYPE_TFTD_USO:
-					button->blitNShade(_bg, 0, 0);
+					if (bpp == 8) {
+						button->blitNShade(_bg, 0, 0);
+					}
+					else {
+						Surface surf;
+						get32Surf("32_" + ruleInterface->getBackgroundImage() + "-InfoButton", ruleInterface->getBackgroundImage() + "-InfoButton", &surf, "PAL_BATTLEPEDIA")->blitNShade32(_bg, 0, 0, -30); //brighten the background
+					}
+
 					break;
 				default:
 					break;
 			}
 		}
 
-		_txtInfo = new Text(defs->text_width, 150, 320 - defs->text_width, 34);
-		_txtTitle = new Text(284, 16, 36, 14);
-
-		add(_txtTitle);
+		_txtInfo = new Text(defs->text_width * scaleX, 150 * scaleY, (320 - defs->text_width) * scaleX, 34 * scaleY, bpp);
+		_txtInfo->setScale(scaleX, scaleY);
 		add(_txtInfo);
+		_txtInfo->setColor(_textColor);
+		_txtInfo->setSecondaryColor(_textColor2);
+		_txtInfo->setWordWrap(true);
+		_txtInfo->setText(tr(defs->getTextForPage(_state->current_page)));
 
+		_txtTitle = new Text(284 * scaleX, 16 * scaleY, 36 * scaleX, 14 * scaleX, bpp);
+		_txtTitle->setScale(scaleX, scaleY);
+		add(_txtTitle);
 		_txtTitle->setColor(_textColor);
 		_txtTitle->setBig();
 		_txtTitle->setWordWrap(true);
 		_txtTitle->setAlign(ALIGN_CENTER);
 		_txtTitle->setText(tr(defs->getTitleForPage(_state->current_page)));
 
-		_txtInfo->setColor(_textColor);
-		_txtInfo->setSecondaryColor(_textColor2);
-		_txtInfo->setWordWrap(true);
-		_txtInfo->setText(tr(defs->getTextForPage(_state->current_page)));
 
 		// all of the above are common to the TFTD articles.
 
